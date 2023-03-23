1bd6a52b0216285dd9338337ee7c47821e638c2a
Sebastian Valbuena <113988804+Valbuena99@users.noreply.github.com>
2023-03-22 23:10:56 -0500

    Commit project

---
+++ src/components/footer.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer-task">
+            <span>Drag and drop to reorder list</span>
+        </div>
+    )
+}
+
+export default Footer;
